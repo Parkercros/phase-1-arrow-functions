574a1701b040925ef7e06ad0ffd53bece33be49d
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-31 16:02:15 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,20 @@
+//  has a function expression called divide: √
+//  divide divides 2000 by 100:    √
+
+function divide(){                              
+    return 2000 / 100;
+  }
+
+//  has an arrow function called square:       √
+//  square arrow function takes one parameter and multiplies it times itself:   √
+
+square = () => {
+    return (2) * (2);
+  }
+
+//  has an arrow function called add:   √
+// /  add arrow function takes two parameters and adds them together:
+
+add = () => {
+    return (2) + (5);
+  }
